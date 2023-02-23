18903a5fe10e114bdfa8d53473e74a7300f97226
Moabe Renato <mrov@cin.ufpe.br>
2023-02-23 10:22:24 -0300

    adding the Dices component

---
+++ src/Dices.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {IDice} from './interfaces/IDice';
+
+type incomingParams = {
+  setDiceResult: Function;
+};
+
+function Dices({setDiceResult}: incomingParams): JSX.Element {
+  let dices: IDice[] = [
+    {
+      title: 'D4',
+      number: 4,
+    },
+    {
+      title: 'D6',
+      number: 6,
+    },
+    {
+      title: 'D8',
+      number: 8,
+    },
+    {
+      title: 'D10',
+      number: 10,
+    },
+    {
+      title: 'D100',
+      number: 100,
+    },
+    {
+      title: 'D12',
+      number: 12,
+    },
+    {
+      title: 'D20',
+      number: 20,
+    },
+  ];
+
+  const rollTheDice = function (max: number, min: number = 1) {
+    for (let index = 0; index < 10; index++) {
+      setTimeout(() => {
+        setDiceResult(Math.floor(Math.random() * (max + 1 - min) + min));
+      }, 75 * index);
+    }
+  };
+
+  return (
+    <View style={styles.diceWrapper}>
+      {dices.map(dice => {
+        return (
+          <View
+            key={dice.title}
+            onTouchEnd={() => {
+              rollTheDice(dice.number);
+            }}
+            style={[styles.dice]}>
+            <Text>{dice.title}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  diceWrapper: {flex: 1, flexDirection: 'row'},
+  dice: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    height: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 2,
+  },
+});
+
+export default Dices;
---
+++ src/interfaces/IDice.ts
@@ -0,0 +1,5 @@
+export interface IDice {
+  title: string;
+  number: number;
+  img?: string;
+}
